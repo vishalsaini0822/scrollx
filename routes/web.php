1c45f868444839e917c6a6531214d5478f02733d
vishalsaini0822 <vishalsaini0822@gmail.com>
2025-04-02 23:10:20 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('welcome', function () {
     return view('welcome');
 });
-Route::get('', function () {
+Route::get('/', function () {
     return view('home');
 });
