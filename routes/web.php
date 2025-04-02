c0a1b221601d0d2732b4a61d274184073944108f
vishalsaini0822 <vishalsaini0822@gmail.com>
2025-04-02 23:20:53 +0530

    create login and signup

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,11 @@ Route::get('welcome', function () {
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('register', function () {
+    return view('register');
+});
