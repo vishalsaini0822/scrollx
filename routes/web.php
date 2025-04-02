159b12e54309cf4cbef8e816f1965dfd40ad6d61
vishalsaini0822 <vishalsaini0822@gmail.com>
2025-04-02 23:08:30 +0530

    create home page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('welcome', function () {
     return view('welcome');
+});
+Route::get('', function () {
+    return view('home');
 });
